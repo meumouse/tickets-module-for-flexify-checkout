5cb0881649f8717b1569ed9c75b66b49e5dab747
MeuMouse.com <contato@meumouse.com>
2025-11-23 21:41:02 -0300

    Added feature: Ticket details on customer emails

--- inc/Admin/Order.php
+++ inc/Admin/Order.php
@@ -23,6 +23,9 @@ class Order {
     public function __construct() {
         // display ticket fields on order details
         add_action( 'woocommerce_admin_order_data_after_billing_address', array( $this, 'display_fields_on_order_details' ), 10, 1 );
+
+        // display ticket fields on customer order
+        add_action( 'woocommerce_email_order_details', array( $this, 'display_fields_on_email' ), 20, 3 );
     }
 
 
@@ -62,6 +65,53 @@ class Order {
             }
 
             echo esc_html__( 'E-mail: ', 'tickets-module-for-flexify-checkout' ) . esc_html( $email ) . '</p>';
+        }
+    }
+
+
+    /**
+     * Display ticket fields on customer emails
+     *
+     * @since 1.2.1
+     * @param WC_Order $order | Order object
+     * @param bool     $sent_to_admin
+     * @param string   $email_class
+     * @return void
+     */
+    public function display_fields_on_email( $order, $sent_to_admin, $email_class ) {
+        // Show only to customer emails (not admin)
+        if ( $sent_to_admin ) {
+            return;
+        }
+
+        if ( 0 === Helpers::ticket_count( $order ) ) {
+            return;
+        }
+
+        echo '<h3>' . esc_html__( 'Informações dos ingressos', 'tickets-module-for-flexify-checkout' ) . '</h3>';
+
+        for ( $i = 1; $i <= Helpers::ticket_count( $order ); $i++ ) {
+
+            $first_name = get_post_meta( $order->get_id(), 'billing_first_name_' . $i, true );
+            $last_name = get_post_meta( $order->get_id(), 'billing_last_name_' . $i, true );
+            $cpf = get_post_meta( $order->get_id(), 'billing_cpf_' . $i, true );
+            $phone = get_post_meta( $order->get_id(), 'billing_phone_' . $i, true );
+            $phone_int = get_post_meta( $order->get_id(), 'billing_phone_' . $i . '_full', true );
+            $email = get_post_meta( $order->get_id(), 'billing_email_' . $i, true );
+
+            echo '<p>';
+                echo '<strong>' . sprintf( __( 'Ingresso %s', 'tickets-module-for-flexify-checkout' ), $i ) . '</strong><br>';
+                echo esc_html__( 'Nome: ', 'tickets-module-for-flexify-checkout' ) . esc_html( $first_name ) . ' ' . esc_html( $last_name ) . '<br>';
+                echo esc_html__( 'CPF: ', 'tickets-module-for-flexify-checkout' ) . esc_html( $cpf ) . '<br>';
+
+                if ( ! empty( $phone_int ) ) {
+                    echo esc_html__( 'Telefone: ', 'tickets-module-for-flexify-checkout' ) . esc_html( $phone_int ) . '<br>';
+                } else {
+                    echo esc_html__( 'Telefone: ', 'tickets-module-for-flexify-checkout' ) . esc_html( $phone ) . '<br>';
+                }
+
+                echo esc_html__( 'E-mail: ', 'tickets-module-for-flexify-checkout' ) . esc_html( $email );
+            echo '</p>';
         }
     }
 }
